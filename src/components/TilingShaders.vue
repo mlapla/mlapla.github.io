9ac95b53474b0297fb9943119042e50ab7399b7e
Michael Laplante <laplante.michael@hotmail.com>
2022-01-17 12:37:49 -0500

    Attempted to integrate tilings

---
+++ src/components/TilingShaders.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="container"></div>
+</template>
+
+<script>
+import init from './show.js';
+export default {
+  mounted () {
+    init()
+  },
+  name: "TilingShaders"
+}
+</script>
+
+<style scoped>
+
+</style>
